e39994e34eb262fed56f2ec22455ceb5b1d6f343
David <David@DAVID-PC>
2025-07-07 21:04:12 -0500

    Agregar archivos de proyecto.

---
+++ Clases/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03.Clases
+{
+    public class Usuario
+    {
+        public int id;
+        public string cedula;
+        public string nombre;
+        public string apellido;
+        public string mail;
+        private string tarjetaCredito;
+
+        public Usuario(string cedula, string nombre, string apellido, string mail, string tarjetaCredito)
+        {
+            this.cedula = cedula;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.mail = mail;
+            this.tarjetaCredito = tarjetaCredito;
+        }
+
+
+        public string getTarjetaCredito1()
+        {
+            Console.WriteLine("Ingrese la cedula: ");
+            string cedulaIngresada = Console.ReadLine();
+            if (cedulaIngresada == this.cedula)
+            {
+                return tarjetaCredito;
+            }
+            else
+            {
+                Console.WriteLine("Cedula incorrecta, no tiene acceso a la tarjeta");
+                return "Cedula incorrecta";
+            }
+
+        }
+
+        public string getTarjetaCredito2()
+        {
+            int cantidadCaracteres = this.tarjetaCredito.Length;
+
+            string retornoTarjetaCredito = this.tarjetaCredito.Replace(tarjetaCredito.Substring(0, cantidadCaracteres - 4), "**");
+
+            return retornoTarjetaCredito;
+
+        }
+
+        public void setTarjetaCredito(string numeroTarjetaCredito)
+        {
+            Console.WriteLine("Ingrese la cedula: ");
+            string cedulaIngresada = Console.ReadLine();
+            if (cedulaIngresada == this.cedula)
+            {
+                this.tarjetaCredito = numeroTarjetaCredito;
+            }
+            else
+            {
+                Console.WriteLine("Cedula incorrecta, no tiene acceso a la tarjeta");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_03.Clases;
+
+namespace Clase03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario ObjUsuario = new Usuario("0950520478", "David", "Aguilar", "davidaguilar@hotmail.com", "12345678901234");
+            Console.WriteLine("Cedula: " + ObjUsuario.cedula);
+            Console.WriteLine("Nombre: " + ObjUsuario.nombre);
+            Console.WriteLine("Apellido: " + ObjUsuario.apellido);
+            Console.WriteLine("Tarjeta: " + ObjUsuario.getTarjetaCredito2());
+            //ObjUsuario.setTarjetaCredito("1234567890");
+            Console.WriteLine("Tarjeta: " + ObjUsuario.getTarjetaCredito2());
+            Console.ReadLine();
+
+
+            instanciarObjeto();
+
+        }
+
+        private static void instanciarObjeto()
+        {
+            Console.Clear();
+            Console.WriteLine("INGRESE UN NUEVO USUARIO");
+            Console.WriteLine();
+            Console.Write("Ingrese la cédula: ");
+            string cedula = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el apellido: ");
+            string apellido = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el mail: ");
+            string mail = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese su tarjeta de crédito: ");
+            string tarjetaCredito = Console.ReadLine();
+            Console.WriteLine();
+
+            Usuario objetoUsuario = new Usuario(cedula, nombre, apellido, mail, tarjetaCredito);
+            Console.WriteLine("La tarjeta de crédito es: " + objetoUsuario.getTarjetaCredito2());
+
+            Console.WriteLine("Ingrese la Nueva Tarjeta de Crédito: ");
+            tarjetaCredito = Console.ReadLine();
+
+            objetoUsuario.setTarjetaCredito(tarjetaCredito);
+            Console.WriteLine("La nueva tarjeta de crédito es: " + objetoUsuario.getTarjetaCredito2());
+
+        }
+    }
+}
